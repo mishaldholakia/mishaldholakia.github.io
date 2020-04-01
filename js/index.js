77ed62f932bf8357e62fd4783cdf747748a688fb
mishaldholakia <mishal.dholakia1@ibm.com>
2020-04-01 08:04:23 -0400

    update the story with nortam data

--- js/index.js
+++ js/index.js
@@ -1,141 +1,92 @@
 const array = require('../data/data.json')
+    // console.log(array);
 mapboxgl.accessToken = 'pk.eyJ1IjoibWlzaGFsZGhvbGFraWEiLCJhIjoiY2pqeXFmaTFpMWNmYTNwbzYzbjF1dDZuMSJ9.nXDFbTcdJPSc4U9Wqv1few';
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/dark-v9',
     center: [-96, 37.8],
     zoom: 3,
-    minZoom: 1
+    minZoom: 0
 });
 
-array.forEach(element => {
-    let origin = element.origin;
-    let destination = element.destination
-    let id = element.id;
-    let color = element.color;
-    let status = element.status;
+// // San Francisco
+// var origin = [-122.414, 37.776];
 
-    // A simple line from origin to destination.
-    var route = {
-        'type': 'FeatureCollection',
-        'features': [{
-            'type': 'Feature',
-            'geometry': {
-                'type': 'LineString',
-                'coordinates': [origin, destination]
-            }
-        }]
-    };
+// // Washington DC
+// var destination = [-77.032, 38.913];
+map.on('load', function() {
+    array.forEach(element => {
+        let origin = [element.lng, element.lat];
+        let id = element.ident;
 
-    // A single point that animates along the route.
-    // Coordinates are initially set to origin.
-    var point = {
-        'type': 'FeatureCollection',
-        'features': [{
-            'type': 'Feature',
-            'properties': {},
-            'geometry': {
-                'type': 'Point',
-                'coordinates': origin
-            }
-        }]
-    };
-
-    // Calculate the distance in kilometers between route start/end point.
-    var lineDistance = turf.lineDistance(route.features[0], 'kilometers');
-
-    var arc = [];
-
-    // Number of steps to use in the arc and animation, more steps means
-    // a smoother arc and animation, but too many steps will result in a
-    // low frame rate
-    var steps = 1200;
-
-    // Draw an arc between the `origin` & `destination` of the two points
-    for (var i = 0; i < lineDistance; i += lineDistance / steps) {
-        var segment = turf.along(route.features[0], i, 'kilometers');
-        arc.push(segment.geometry.coordinates);
-    }
-
-    // Update the route with calculated arc coordinates
-    route.features[0].geometry.coordinates = arc;
-
-    // Used to increment the value of the point measurement against the route.
-    // var counter = 0;
-
-    map.on('load', function() {
-        // Add a source and layer displaying a point which will be animated in a circle.
-        map.addSource(`${id} + ${status}`, {
-            'type': 'geojson',
-            'data': route
-        });
-
-        map.addSource(`${id} + point`, {
+        // A single point that animates along the route.
+        // Coordinates are initially set to origin.
+        var point = {
+            'type': 'FeatureCollection',
+            'features': [{
+                'type': 'Feature',
+                'properties': {},
+                'geometry': {
+                    'type': 'Point',
+                    'coordinates': origin
+                }
+            }]
+        };
+        map.addSource(`${id}`, {
             'type': 'geojson',
             'data': point
         });
-
         map.addLayer({
-            'id': `${id} + ${status}`,
-            'source': `${id} + ${status}`,
-            'type': 'line',
-            'layout': {
-                'visibility': 'visible'
-            },
-            'paint': {
-                'line-width': 1,
-                'line-color': color || '#' + (Math.random().toString(16) + "000000").substring(2, 8)
-            }
-            //'line-color': '#' + (Math.random().toString(16) + "000000").substring(2, 8)
+                'id': `${id}`,
+                'source': `${id}`,
+                'type': 'circle',
+                'paint': {
+                    // make circles larger as the user zooms from z12 to z22
+                    'circle-radius': {
+                        'base': 20,
+                        'stops': [
+                            [12, 2],
+                            [22, 180]
+                        ]
+                    },
+                    "circle-stroke-width": 3,
+                    "circle-stroke-color": '#3bb2d0',
+                    "circle-color": '#3bb2d0'
+                }
+            })
+            // Create a popup, but don't add it to the map yet.
+        var popup = new mapboxgl.Popup({
+            closeButton: false,
+            closeOnClick: false
         });
-        map.addLayer({
-            'id': `${id} + point`,
-            'source': `${id} + point`,
-            'type': 'symbol',
-            'layout': {
-                'icon-image': 'airport-15',
-                'icon-rotate': ['get', 'bearing'],
-                'icon-rotation-alignment': 'map',
-                'icon-allow-overlap': true,
-                'icon-ignore-placement': true
-            }
+
+        map.on('mouseenter', `${id}`, function(e) {
+            // Change the cursor style as a UI indicator.
+            map.getCanvas().style.cursor = 'pointer';
+
+            // let description = element.text;
+
+            let description = `<strong>${element.ident}</strong <br><p>${element.text}</p>`;
+
+            // Ensure that if the map is zoomed out such that multiple
+            // copies of the feature are visible, the popup appears
+            // over the copy being pointed to.
+            // while (Math.abs(element.lng - element.lat) > 180) {
+            //     origin[0] += element.lng > element.lat ? 360 : -360;
+            // }
+
+            // Populate the popup and set its coordinates
+            // based on the feature found.
+            popup
+                .setLngLat(origin)
+                .setHTML(element.ident)
+                .setHTML(description)
+                .addTo(map);
+        });
+
+        map.on('mouseleave', `${id}`, function() {
+            map.getCanvas().style.cursor = '';
+            popup.remove();
         });
     })
-})
-
-document.getElementById('bx--checkbox-active').addEventListener('click', function() {
-    let status = document.getElementById('bx--checkbox-active');
-    if (status.checked) {
-        array.forEach(element => {
-            if (element.status === 'active') {
-                map.setLayoutProperty(`${element.id} + ${element.status}`, 'visibility', 'visible');
-                map.setLayoutProperty(`${element.id} + point`, 'visibility', 'visible');
-            }
-        })
-    } else {
-        array.forEach(element => {
-            if (element.status === 'active') {
-                map.setLayoutProperty(`${element.id} + ${element.status}`, 'visibility', 'none');
-                map.setLayoutProperty(`${element.id} + point`, 'visibility', 'none');
-            }
-        })
-    }
-});
-document.getElementById('bx--checkbox-inactive').addEventListener('click', function() {
-    let status = document.getElementById('bx--checkbox-inactive');
-    if (status.checked) {
-        array.forEach(element => {
-            if (element.status === 'inactive') {
-                map.setLayoutProperty(`${element.id} + ${element.status}`, 'visibility', 'visible');
-                map.setLayoutProperty(`${element.id} + point`, 'visibility', 'visible');
-            }
-        })
-    } else {
-        array.forEach(element => {
-            if (element.status === 'inactive') {
-                map.setLayoutProperty(`${element.id} + ${element.status}`, 'visibility', 'none');
-                map.setLayoutProperty(`${element.id} + point`, 'visibility', 'none');
-            }
-        })
-    }
 });
