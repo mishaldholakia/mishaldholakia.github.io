f96ea328f9d1d112b6a104a9aaa285546962f9b3
mishaldholakia <mishal.dholakia1@ibm.com>
2020-04-02 03:12:39 -0400

    update to include the travel restriction

--- js/index.js
+++ js/index.js
@@ -1,92 +1,61 @@
-const array = require('../data/data.json')
-    // console.log(array);
+const json = require('../data/restricted-travel-map/data.json')
 mapboxgl.accessToken = 'pk.eyJ1IjoibWlzaGFsZGhvbGFraWEiLCJhIjoiY2pqeXFmaTFpMWNmYTNwbzYzbjF1dDZuMSJ9.nXDFbTcdJPSc4U9Wqv1few';
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/dark-v9',
+    style: 'mapbox://styles/mapbox/light-v9',
     center: [-96, 37.8],
-    zoom: 3,
-    minZoom: 0
-});
-
-// // San Francisco
-// var origin = [-122.414, 37.776];
-
-// // Washington DC
-// var destination = [-77.032, 38.913];
+    zoom: 1,
+    minZoom: 1
+})
 map.on('load', function() {
-    array.forEach(element => {
-        let origin = [element.lng, element.lat];
-        let id = element.ident;
+    // Add a source for the state polygons.
+    map.addSource('countries', {
+        'type': 'geojson',
+        'data': 'https://datahub.io/core/geo-countries/r/countries.geojson'
+    });
 
-        // A single point that animates along the route.
-        // Coordinates are initially set to origin.
-        var point = {
-            'type': 'FeatureCollection',
-            'features': [{
-                'type': 'Feature',
-                'properties': {},
-                'geometry': {
-                    'type': 'Point',
-                    'coordinates': origin
-                }
-            }]
-        };
-        map.addSource(`${id}`, {
-            'type': 'geojson',
-            'data': point
+    // Add a layer showing the state polygons.
+    map.addLayer({
+        'id': 'states-layer',
+        'type': 'fill',
+        'source': 'countries',
+        'paint': {
+            'fill-color': '#0f6dff',
+            'fill-opacity': 0.5
+        }
+    });
+
+    var popup = new mapboxgl.Popup({
+        closeButton: false,
+        closeOnClick: false
+    });
+
+    // When a click event occurs on a feature in the states layer, open a popup at the
+    // location of the click, with description HTML from its properties.
+    map.on('mouseenter', 'states-layer', function(e) {
+        json.forEach(element => {
+            if (e.features[0].properties.ISO_A3 === element.ISO_A3) {
+                let description = `<h4>${e.features[0].properties.ADMIN}</h4><br><ul><li>isInboundRestricted:${element.isInboundRestricted}</li><li>isInboundLandRestricted:${element.isInboundLandRestricted}</li><li>isInboundCompletelyRestricted:${element.isInboundCompletelyRestricted}</li><li>inboundRestrictedCountryNamesRaw:${element.inboundRestrictedCountryNamesRaw}</li>`;
+                popup
+                    .setLngLat(e.lngLat)
+                    .setHTML(description)
+                    .addTo(map);
+            }
         });
-        map.addLayer({
-                'id': `${id}`,
-                'source': `${id}`,
-                'type': 'circle',
-                'paint': {
-                    // make circles larger as the user zooms from z12 to z22
-                    'circle-radius': {
-                        'base': 20,
-                        'stops': [
-                            [12, 2],
-                            [22, 180]
-                        ]
-                    },
-                    "circle-stroke-width": 3,
-                    "circle-stroke-color": '#3bb2d0',
-                    "circle-color": '#3bb2d0'
-                }
-            })
-            // Create a popup, but don't add it to the map yet.
-        var popup = new mapboxgl.Popup({
-            closeButton: false,
-            closeOnClick: false
-        });
+        // popup
+        //     .setLngLat(e.lngLat)
+        //     .setHTML(e.features[0].properties.ADMIN)
+        //     .addTo(map);
+    });
 
-        map.on('mouseenter', `${id}`, function(e) {
-            // Change the cursor style as a UI indicator.
-            map.getCanvas().style.cursor = 'pointer';
+    // Change the cursor to a pointer when the mouse is over the states layer.
+    map.on('mouseenter', 'states-layer', function() {
+        map.getCanvas().style.cursor = 'pointer';
+    });
 
-            // let description = element.text;
-
-            let description = `<b>${element.ident}</b> <br><p>${element.text}</p>`;
-
-            // Ensure that if the map is zoomed out such that multiple
-            // copies of the feature are visible, the popup appears
-            // over the copy being pointed to.
-            // while (Math.abs(element.lng - element.lat) > 180) {
-            //     origin[0] += element.lng > element.lat ? 360 : -360;
-            // }
-
-            // Populate the popup and set its coordinates
-            // based on the feature found.
-            popup
-                .setLngLat(origin)
-                .setHTML(element.ident)
-                .setHTML(description)
-                .addTo(map);
-        });
-
-        map.on('mouseleave', `${id}`, function() {
-            map.getCanvas().style.cursor = '';
-            popup.remove();
-        });
-    })
+    // Change it back to a pointer when it leaves.
+    map.on('mouseleave', 'states-layer', function() {
+        popup.remove();
+        map.getCanvas().style.cursor = '';
+    });
 });
